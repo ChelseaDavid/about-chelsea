7eb8c8bc97c0cec938b85e55eb62478e1830c83d
Chelsea David <cryan17@cnm.edu>
2017-07-10 19:34:19 -0600

    added starter index page with images

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		<h1><strong>Meet Chelsea</strong></h1>
 
 		<p> Hello! My name is Chelsea</p>
-		<p>I will be turning 28 on Sun!!  I come from the world of flooring and construction. For the last 5 years I was the Operations & Warehouse manager for Interior Soulutions Inc(formerly HD Supply, formerly CTI, formerly Home Depot). We're a lot of formerly's.
+		<p>I will be turning 28 on Sun!!  I come from the world of flooring and construction. For the last 5 years I was the Operations and Warehouse manager for Interior Soulutions Inc(formerly HD Supply, formerly CTI, formerly Home Depot). We're a lot of formerly's.
 			They do the flooring and interior design for Pulte, DRH, and Abrazo, just to name a few. I was initially brought on as a temp and in just a few short months, was brought on full time tackling a 2 person job</p>
 		<p>I am fork lift certified as well as a beast when it comes to unloading pad trucks. Something I was always quite proud of!</p>
 		<p>While I was sad to leave the world I have known for the last 5 years, I am thrilled to be embarking on a new chapter in my life which i hope will take me to bigger and better things!!</p>
