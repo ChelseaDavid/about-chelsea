2c542895177a24be16a6281a5c1019a8d7271d6d
Chelsea David <cryan17@cnm.edu>
2017-07-10 19:33:46 -0600

    added starter index page with images

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>About Chelsea</title>
+	</head>
+
+	<body>
+
+		<h1><strong>Meet Chelsea</strong></h1>
+
+		<p> Hello! My name is Chelsea</p>
+		<p>I will be turning 28 on Sun!!  I come from the world of flooring and construction. For the last 5 years I was the Operations & Warehouse manager for Interior Soulutions Inc(formerly HD Supply, formerly CTI, formerly Home Depot). We're a lot of formerly's.
+			They do the flooring and interior design for Pulte, DRH, and Abrazo, just to name a few. I was initially brought on as a temp and in just a few short months, was brought on full time tackling a 2 person job</p>
+		<p>I am fork lift certified as well as a beast when it comes to unloading pad trucks. Something I was always quite proud of!</p>
+		<p>While I was sad to leave the world I have known for the last 5 years, I am thrilled to be embarking on a new chapter in my life which i hope will take me to bigger and better things!!</p>
+		<p>While work these last 5 years has seemed to consume my every second, I have not had much down-time. That said, when I CAN make the time, I enjoy archery, roller derby, and video gaming!</p>
+
+		<img src="images/evilqueenandherstepsisters.jpg" alt="evilqueen">
+		<img src="images/breakingground.jpg" alt="breakingground">
+		<img src="images/branchofthequater.jpg" alt="branchofthequater">
+		<img src="images/papaduex.jpg" alt="papaduex">
+		<img src="images/work.jpg" alt="work">
+
+		
+	</body>
+</html>
